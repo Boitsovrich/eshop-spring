23a8905ee66ed8cf71ad039bcfe13db9fed9acd6
aboytsov <aboytsov@phoenixit.ru>
2022-01-14 20:55:04 +0300

    Revert "test"
    This reverts commit 7c6f0130013f2e3fffc15cb53463823679e62c72.

--- src/main/java/com/example/eshopspring/EshopSpringApplication.java
+++ src/main/java/com/example/eshopspring/EshopSpringApplication.java
@@ -8,7 +8,6 @@ public class EshopSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EshopSpringApplication.class, args);
-        System.out.println("test");
     }
 
 }
